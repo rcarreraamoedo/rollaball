a4507ad8393176e97c5365682dbcb3fce40e00ac
Rubén Carrera Amoedo <rcarreraamoedo@danielcastelao.org>
2021-02-06 13:36:57 +0100

    Enemy y descuento

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -58,16 +58,19 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (SystemInfo.deviceType == DeviceType.Desktop)
-        {
-            Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-            rb.AddForce(movement * speed);
-        }
-        else
-        {
-            Vector3 movement = new Vector3(Input.acceleration.x, 0.0f, Input.acceleration.y);
-            rb.AddForce(movement * speed);
-        }
+       // para el teclado
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+
+        // recogemos los datos del acelerometro
+        Vector3 dir = Vector3.zero;
+        dir.x = -Input.acceleration.y;
+        dir.z = Input.acceleration.x;
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+        
+        dir *= Time.deltaTime;
+        transform.Translate(dir * speed);
         
     }
 
